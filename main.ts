8580d8581dab06520ecb046c785828c30415a8d4
Ondrejoda <54633773+Ondrejoda@users.noreply.github.com>
2022-12-10 14:59:48 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-let pressed = false
+let just_pressed = false
+let just_released = false
 
 let previous_state = false
 let current_state = false
@@ -7,28 +8,41 @@ let start_time = 0.0
 let end_time = 0.0
 let measuring = false
 
-let measure = () => {
-    let rtn = 0.0
-    if (!measuring) {
-        start_time = control.millis()
-    } else {
-        end_time = control.millis()
-        rtn = end_time - start_time
-    }
-    measuring = !measuring
-    return rtn
-}
-
 let detect_change = () => {
     return current_state != previous_state
 }
 
-basic.forever(function () {
+let start_measure = () => {
+    start_time = control.millis()
+}
+
+let end_measure = () => {
+    end_time = control.millis()
+    return end_time - start_time
+}
+
+let handle_button = () => {
+    just_pressed = false
+    just_released = false
     current_state = input.buttonIsPressed(Button.A)
 
-    if (detect_change()) {
-        pressed = current_state
+    if (detect_change() && current_state) {
+        just_pressed = true
+    }
+    if (detect_change() && !current_state) {
+        just_released = true
     }
 
     previous_state = current_state
+}
+
+basic.forever(function () {
+    handle_button()
+    if (just_pressed) {
+        start_measure()
+    }
+    if (just_released) {
+        basic.showNumber(Math.round(end_measure() / 1000))
+    }
+
 })
