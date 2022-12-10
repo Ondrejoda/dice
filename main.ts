48330ee822f9848e924df7bb98d8adeaf38febbc
Ondrejoda <54633773+Ondrejoda@users.noreply.github.com>
2022-12-10 14:42:22 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+let pressed = false
+
+let previous_state = false
+let current_state = false
+
+let start_time = 0.0
+let end_time = 0.0
+let measuring = false
+
+let measure = () => {
+    let rtn = 0.0
+    if (!measuring) {
+        start_time = control.millis()
+    } else {
+        end_time = control.millis()
+        rtn = end_time - start_time
+    }
+    measuring = !measuring
+    return rtn
+}
+
+let detect_change = () => {
+    return current_state != previous_state
+}
+
 basic.forever(function () {
-	
+    current_state = input.buttonIsPressed(Button.A)
+
+    if (detect_change()) {
+        pressed = current_state
+    }
+
+    previous_state = current_state
 })
